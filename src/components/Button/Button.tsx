0f5cc61f707cf5f1c511786d7f10a21e8d580a74
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-09-12 21:14:38 +0300

    REACT-project-practice

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,21 @@
+// export default function Button() {
+//   const handleClick = () => {
+//     console.log("I'm a button!");
+//   };
+
+import type React from "react";
+
+//   return <button onClick={handleClick}>Click me!</button>;
+// }
+
 export default function Button() {
-  const handleClick = () => {
-    console.log("Button clicked!");
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event);
   };
-  return <button onClick={() => console.log("Clicked!")}>Click Me</button>;
-  //   return <button onClick={handleClick}>Click Me</button>;
+  return (
+    <>
+      <button onClick={handleClick}></button>
+      <button onClick={event}>Click me!</button>;
+    </>
+  );
 }
