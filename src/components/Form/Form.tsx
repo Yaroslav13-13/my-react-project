0c8d5aff2dbe5da3077ade2ac7e524567799a234
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-09-14 00:44:24 +0300

    REACT-project-practice

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -16,13 +16,39 @@
 //     </form>
 //   );
 // }
+
+import { useState } from "react";
 export default function Form() {
-  const handleSubmit = (event: React.MauseEvent<HTMLFormElement>) => {
-    alert("Form submitted");
+  const [username, setUsername] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
   };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    alert(`Username: ${username}`);
+  };
+
   return (
-    <div>
-      <button onClick={handleSubmit}>FUCK</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="username"
+        value={username}
+        onChange={handleChange}
+        placeholder="Введи імʼя"
+        style={{
+          height: "35px",
+          marginRight: "8px",
+          fontSize: "16px",
+          paddingLeft: "8px",
+          color: "#d82424ff",
+          border: "1px solid #000000ff",
+          borderRadius: "9px",
+        }}
+      />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
