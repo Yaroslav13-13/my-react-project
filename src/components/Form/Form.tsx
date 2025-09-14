7c8da86207f26b01e47cad3f10ed78ebdc13ef7f
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-09-14 12:31:51 +0300

    REACT-project-practice

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -17,38 +17,38 @@
 //   );
 // }
 
-import { useState } from "react";
-export default function Form() {
-  const [username, setUsername] = useState("");
+// import { useState } from "react";
+// export default function Form() {
+//   const [username, setUsername] = useState("");
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
-  };
+//   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     setUsername(event.target.value);
+//   };
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    alert(`Username: ${username}`);
-  };
+//   const handleSubmit = (event: React.FormEvent) => {
+//     event.preventDefault();
+//     alert(`Username: ${username}`);
+//   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="username"
-        value={username}
-        onChange={handleChange}
-        placeholder="Введи імʼя"
-        style={{
-          height: "35px",
-          marginRight: "8px",
-          fontSize: "16px",
-          paddingLeft: "8px",
-          color: "#d82424ff",
-          border: "1px solid #000000ff",
-          borderRadius: "9px",
-        }}
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input
+//         type="text"
+//         name="username"
+//         value={username}
+//         onChange={handleChange}
+//         placeholder="Введи імʼя"
+//         style={{
+//           height: "35px",
+//           marginRight: "8px",
+//           fontSize: "16px",
+//           paddingLeft: "8px",
+//           color: "#d82424ff",
+//           border: "1px solid #000000ff",
+//           borderRadius: "9px",
+//         }}
+//       />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );я
+// }
